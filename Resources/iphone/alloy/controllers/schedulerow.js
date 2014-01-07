54d6c38da997051c8c59093526e51ec95c357a97
Martin de Keijzer <martin@dikketoeters.nl>
2014-01-07 22:30:36 +0100

    schedule date showing

--- Resources/iphone/alloy/controllers/schedulerow.js
+++ Resources/iphone/alloy/controllers/schedulerow.js
@@ -14,15 +14,19 @@ function Controller() {
     var exports = {};
     var __defers = {};
     $.__views.schedulerow = Ti.UI.createTableViewRow({
+        height: Ti.UI.SIZE,
+        layout: "horizontal",
         id: "schedulerow"
     });
     $.__views.schedulerow && $.addTopLevelView($.__views.schedulerow);
     openDetail ? $.__views.schedulerow.addEventListener("click", openDetail) : __defers["$.__views.schedulerow!click!openDetail"] = true;
-    $.__views.__alloyId13 = Ti.UI.createView({
-        layout: "vertical",
-        id: "__alloyId13"
+    $.__views.timing = Ti.UI.createView({
+        height: Ti.UI.SIZE,
+        left: 0,
+        id: "timing",
+        layout: "vertical"
     });
-    $.__views.schedulerow.add($.__views.__alloyId13);
+    $.__views.schedulerow.add($.__views.timing);
     $.__views.startTimeLabel = Ti.UI.createLabel({
         width: Ti.UI.SIZE,
         height: Ti.UI.SIZE,
@@ -34,8 +38,8 @@ function Controller() {
         },
         id: "startTimeLabel"
     });
-    $.__views.__alloyId13.add($.__views.startTimeLabel);
-    $.__views.__alloyId14 = Ti.UI.createLabel({
+    $.__views.timing.add($.__views.startTimeLabel);
+    $.__views.__alloyId13 = Ti.UI.createLabel({
         width: Ti.UI.SIZE,
         height: Ti.UI.SIZE,
         color: "#000",
@@ -45,9 +49,9 @@ function Controller() {
             fontWeight: "bold"
         },
         text: "-",
-        id: "__alloyId14"
+        id: "__alloyId13"
     });
-    $.__views.__alloyId13.add($.__views.__alloyId14);
+    $.__views.timing.add($.__views.__alloyId13);
     $.__views.endTimeLabel = Ti.UI.createLabel({
         width: Ti.UI.SIZE,
         height: Ti.UI.SIZE,
@@ -59,12 +63,14 @@ function Controller() {
         },
         id: "endTimeLabel"
     });
-    $.__views.__alloyId13.add($.__views.endTimeLabel);
-    $.__views.__alloyId15 = Ti.UI.createView({
-        layout: "vertical",
-        id: "__alloyId15"
+    $.__views.timing.add($.__views.endTimeLabel);
+    $.__views.session = Ti.UI.createView({
+        height: Ti.UI.SIZE,
+        left: 0,
+        id: "session",
+        layout: "vertical"
     });
-    $.__views.schedulerow.add($.__views.__alloyId15);
+    $.__views.schedulerow.add($.__views.session);
     $.__views.titleLabel = Ti.UI.createLabel({
         width: Ti.UI.FILL,
         height: Ti.UI.SIZE,
@@ -77,7 +83,7 @@ function Controller() {
         },
         id: "titleLabel"
     });
-    $.__views.__alloyId15.add($.__views.titleLabel);
+    $.__views.session.add($.__views.titleLabel);
     $.__views.speakerLabel = Ti.UI.createLabel({
         width: Ti.UI.FILL,
         height: Ti.UI.SIZE,
@@ -90,13 +96,16 @@ function Controller() {
         },
         id: "speakerLabel"
     });
-    $.__views.__alloyId15.add($.__views.speakerLabel);
+    $.__views.session.add($.__views.speakerLabel);
     exports.destroy = function() {};
     _.extend($, $.__views);
     var args = arguments[0] || {};
+    var moment = require("alloy/moment");
+    var startDate = moment(args.startDate);
+    moment(args.endDate);
     $.titleLabel.text = args.title;
-    $.startTimeLabel.text = args.startDate;
-    $.endTimeLabel.text = args.endDate;
+    $.startTimeLabel.text = startDate.format("DD MMM, HH:mm");
+    $.endTimeLabel.text = startDate.format("DD MMM, HH:mm");
     $.speakerLabel.text = "speaker";
     __defers["$.__views.schedulerow!click!openDetail"] && $.__views.schedulerow.addEventListener("click", openDetail);
     _.extend($, exports);
--- app/controllers/schedulerow.js
+++ app/controllers/schedulerow.js
@@ -1,12 +1,15 @@
 var args = arguments[0] || {};
-
+var moment = require('alloy/moment');
 
 function openDetail(e) {
     var scheduleDetailWin = Alloy.createController('scheduledetail', args).getView();
     Alloy.Globals.navWindow.openWindow(scheduleDetailWin, {animated:true});
 }
 
+var startDate = moment(args.startDate);
+var endDate = moment(args.endDate);
+
 $.titleLabel.text = args.title;
-$.startTimeLabel.text = args.startDate;
-$.endTimeLabel.text = args.endDate;
+$.startTimeLabel.text = startDate.format('DD MMM, HH:mm');
+$.endTimeLabel.text = startDate.format('DD MMM, HH:mm');
 $.speakerLabel.text = 'speaker';
