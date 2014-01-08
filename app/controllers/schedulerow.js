5fed8921ba120742e9355b8826d060d03e3849b7
Martin de Keijzer <martin.dekeijzer@gmail.com>
2014-01-08 09:24:36 +0100

    show room in the schedule list item, dates and times should go into groupheaders

--- app/controllers/schedulerow.js
+++ app/controllers/schedulerow.js
@@ -1,14 +1,10 @@
 var args = arguments[0] || {};
-var moment = require('alloy/moment');
 
 function openDetail(e) {
     var scheduleDetailWin = Alloy.createController('scheduledetail', args).getView();
     Alloy.Globals.navWindow.openWindow(scheduleDetailWin, {animated:true});
 }
-var startDate = moment(args.startDate);
-var endDate = moment(args.endDate);
 
+$.roomLabel.text = args.room;
 $.titleLabel.text = args.title;
-$.startTimeLabel.text = startDate.format('DD MMM, HH:mm');
-$.endTimeLabel.text = startDate.format('DD MMM, HH:mm');
-$.speakerLabel.text = 'speaker';
+$.speakerLabel.text = args.speaker;
