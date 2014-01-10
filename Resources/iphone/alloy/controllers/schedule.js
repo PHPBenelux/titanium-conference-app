12b44d3160427d5d6b929d85283702a34b957495
Martin de Keijzer <martin@dikketoeters.nl>
2014-01-11 00:47:28 +0100

    loading schedule using collections

--- Resources/iphone/alloy/controllers/schedule.js
+++ Resources/iphone/alloy/controllers/schedule.js
@@ -7,6 +7,41 @@ function Controller() {
         for (var i in sortedKeys) sortedObj[sortedKeys[i]] = arr[sortedKeys[i]];
         return sortedObj;
     }
+    function loadSchedule(collection) {
+        var schedule = collection.toJSON();
+        if (0 == schedule.length) {
+            $.table.headerTitle = "No data";
+            return true;
+        }
+        var sectionSchedule = [];
+        var sections = [];
+        for (var i = 0, iLen = schedule.length; iLen > i; i++) {
+            var timestampKey = moment(schedule[i].startDate).format("X");
+            sectionSchedule[timestampKey] || (sectionSchedule[timestampKey] = []);
+            sectionSchedule[timestampKey].push(Alloy.createController("schedulerow", {
+                title: decoder.decode(schedule[i].title),
+                content: schedule[i].content,
+                speaker: schedule[i].speaker,
+                bio: schedule[i].bio,
+                picture: schedule[i].picture,
+                startDate: schedule[i].startDate,
+                endDate: schedule[i].endDate,
+                room: schedule[i].room,
+                level: schedule[i].level,
+                type: schedule[i].type
+            }).getView());
+        }
+        sectionSchedule = sortObj(sectionSchedule);
+        for (var jIndex in sectionSchedule) {
+            var currentSection = sectionSchedule[jIndex];
+            var sectionHeader = Ti.UI.createTableViewSection({
+                headerTitle: moment(jIndex.toString(), "X").format("DD MMM HH:mm")
+            });
+            for (var k = 0, kLen = currentSection.length; kLen > k; k++) sectionHeader.add(currentSection[k]);
+            sections.push(sectionHeader);
+        }
+        $.table.setData(sections);
+    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "schedule";
     arguments[0] ? arguments[0]["__parentSymbol"] : null;
@@ -14,6 +49,7 @@ function Controller() {
     arguments[0] ? arguments[0]["__itemTemplate"] : null;
     var $ = this;
     var exports = {};
+    Alloy.Collections.instance("schedule");
     $.__views.scheduleWindow = Ti.UI.createWindow({
         fullscreen: true,
         backgroundColor: "white",
@@ -31,52 +67,9 @@ function Controller() {
     arguments[0] || {};
     var moment = require("alloy/moment");
     var decoder = require("entitydecoder");
-    var httpClient = Ti.Network.createHTTPClient({
-        onerror: function(e) {
-            Ti.API.debug(e.error);
-            alert("Unable to retrieve the data");
-        }
+    Alloy.Collections.schedule.fetch({
+        success: loadSchedule
     });
-    httpClient.open("GET", Alloy.CFG.apiUrl + "phpbenelux/schedule");
-    httpClient.send();
-    httpClient.onload = function() {
-        var json = JSON.parse(this.responseText);
-        0 == json.length && ($.table.headerTitle = "No data");
-        var schedule = json.posts;
-        var sectionSchedule = [];
-        var sections = [];
-        for (var i = 0, iLen = schedule.length; iLen > i; i++) {
-            0 != schedule[i].speaker.length && schedule[i].speaker || (schedule[i].speaker = [ {
-                post_title: "PHPBenelux",
-                post_content: "",
-                picture_src: ""
-            } ]);
-            var timestampKey = moment(schedule[i].timestamp_start).format("X");
-            sectionSchedule[timestampKey] || (sectionSchedule[timestampKey] = []);
-            sectionSchedule[timestampKey].push(Alloy.createController("schedulerow", {
-                title: decoder.decode(schedule[i].title),
-                content: schedule[i].content,
-                speaker: schedule[i].speaker[0].post_title,
-                bio: schedule[i].speaker[0].post_content,
-                picture: schedule[i].speaker[0].picture_src,
-                startDate: schedule[i].timestamp_start,
-                endDate: schedule[i].timestamp_end,
-                room: schedule[i].room.post_title,
-                level: schedule[i].talk_level,
-                type: schedule[i].talk_type
-            }).getView());
-        }
-        sectionSchedule = sortObj(sectionSchedule);
-        for (var jIndex in sectionSchedule) {
-            var currentSection = sectionSchedule[jIndex];
-            var sectionHeader = Ti.UI.createTableViewSection({
-                headerTitle: moment(jIndex.toString(), "X").format("DD MMM HH:mm")
-            });
-            for (var k = 0, kLen = currentSection.length; kLen > k; k++) sectionHeader.add(currentSection[k]);
-            sections.push(sectionHeader);
-        }
-        $.table.setData(sections);
-    };
     _.extend($, exports);
 }
 
--- app/models/schedule.js
+++ app/models/schedule.js
@@ -16,7 +16,38 @@ exports.definition = {
 		URL: 'http://conference.phpbenelux.eu/2014/api/phpbenelux/schedule',
 		debug: 0,
 		useStrictValidation: 0,
-		parentNode: "posts",
+		parentNode: function(data) {
+		    var entries = [];
+		    
+		    _.each(data.posts, function(_entry){
+		        
+                // Handle items without an assigned speaker     
+                if (_entry.speaker.length == 0 || !_entry.speaker) {
+                    _entry.speaker = [{
+                        post_title: 'PHPBenelux',
+                        post_content: '',
+                        picture_src: ''
+                    }];
+                }
+                
+		        var post = {};
+		        post.id = _entry.id;
+		        post.title = _entry.title,
+                post.content = _entry.content,
+                post.speaker = _entry.speaker[0].post_title,
+                post.bio = _entry.speaker[0].post_content,
+                post.picture = _entry.speaker[0].picture_src,
+                post.startDate = _entry.timestamp_start,
+                post.endDate = _entry.timestamp_end,
+                post.room = _entry.room.post_title,
+                post.level = _entry.talk_level,
+                post.type = _entry.talk_type,
+		        
+		        entries.push(post);
+		    });
+		    
+		    return entries;
+		},
 		adapter: {
 			idAttribute: "id",
 			type: "sqlrest",
