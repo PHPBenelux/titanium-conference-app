e3ae9b1a67197853db785cd52df1ff2de97c0679
Jeroen van Dijk <jeroen@enrise.com>
2015-01-01 21:29:42 +0100

    Update iOS look&feel

--- app/alloy.js
+++ app/alloy.js
@@ -10,3 +10,9 @@
 //
 // Alloy.Globals.someGlobalFunction = function(){};
 
+(function() {
+
+    // For iOS7 only, set the window's top to 20 so they start under the status bar.
+    Alloy.Globals.windowTop = (OS_IOS && parseInt(Ti.Platform.version[0], 10) >= 7) ? 20 : 0;
+})();
+
