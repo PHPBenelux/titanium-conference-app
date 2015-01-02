ffb2a18c5524be129b795e52066de2f83ee41612
Martin de Keijzer <mdkeijzer@ibuildings.nl>
2015-01-02 12:34:33 +0100

    Use custom bio field

--- app/controllers/speaker.js
+++ app/controllers/speaker.js
@@ -1,5 +1,5 @@
 var args = arguments[0] || {};
 
 $.nameLabel.text = args.name;
-$.bioLabel.text = args.bio.replace(/(<([^>]+)>)/ig,"");
-$.pictureView.image = args.image_75;
+$.bioLabel.text = args.speaker_bio.replace(/(<([^>]+)>)/ig,"");
+$.pictureView.image = args.image_300;
