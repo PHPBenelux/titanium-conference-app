a372fd7cf209bcbcf943a2b9b650c620cdf93f26
Martin de Keijzer <mdkeijzer@ibuildings.nl>
2015-01-02 10:40:55 +0100

    Strip html from abstract text

--- app/controllers/scheduledetail.js
+++ app/controllers/scheduledetail.js
@@ -1,15 +1,17 @@
 var args = arguments[0] || {},
 	moment = require('alloy/moment'),
 	imagecache = require('imagecache'),
-	dispatcher = require('dispatcher');
+	dispatcher = require('dispatcher'),
+	abstract = args.get('content');
 
 function closeWindow(e) {
     $.scheduleDetailWindow.closeWindow();
 }
 imagecache.cachedImageView('speakerimages', args.picture, $.pictureView);
+abstract = abstract.replace(/(<([^>]+)>)/ig,"");
 
 $.titleLabel.text = args.get('title');
-$.descriptionLabel.text = args.get('content');
+$.descriptionLabel.text = abstract;
 $.nameLabel.text = args.speaker;
 $.bioLabel.text = args.bio;
 $.dateLabel.text = moment(args.get('startDate')).format('DD MMM HH:mm') + " - " + moment(args.get('startDate')).format('HH:mm');
