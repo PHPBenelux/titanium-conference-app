d8c21288a11ee2a0c68f4aa7e3d2050a6a75e8b3
Martin de Keijzer <mdkeijzer@ibuildings.nl>
2015-01-02 15:12:22 +0100

    Use listview for news

--- app/controllers/news.js
+++ app/controllers/news.js
@@ -3,22 +3,47 @@ var args = arguments[0] || {},
     decoder = require('entitydecoder'),
     dispatcher = require('dispatcher');
 
-function cleanData(model) {
-	var transform = model.toJSON();
-	transform.title = decoder.decode(transform.title);
-	transform.date = moment(transform.date).format('DD MMM YYYY, HH:mm');
-	return transform;
+function cleanData(data) {
+    var item = {
+        properties: {
+            itemId: data.id
+        },
+        template: 'newsRow',
+        title: {
+            text: 'undefined' !== typeof data.get('title') ? decoder.decode(data.get('title')) : ''
+        },
+        date: {
+            text: 'undefined' !== typeof data.get('date') ? moment(data.get('date')).format('DD MMM YYYY, HH:mm') : ''
+        }
+    };
+    return item;
 }
 
 var hideActivity = function () {
 	$.newsWindow.remove($.activityIndicator);
 };
 
-$.table.addEventListener('click', function(e) {
-	var modelData = Alloy.Collections.news.get(e.rowData.model).toJSON();
-    var newsDetailWin = Alloy.createController('newsdetail', modelData).getView();
+function openDetail(e) {
+    var item = Alloy.Collections.news.get(e.itemId),
+    	newsDetailWin = Alloy.createController('newsdetail', Alloy.Collections.news.get(e.itemId)).getView();
+
     Alloy.Globals.mainView.contentView.add(newsDetailWin);
-});
+};
+
+function loadNews(collection, response, options) {
+	hideActivity();
+	var section = Ti.UI.createListSection({}),
+		newsItems = [],
+        sections = [];
+
+	_.each(collection.models, function(item, index) {
+		newsItems.push(cleanData(item));	
+	});
+        
+	section.setItems(newsItems);
+	sections.push(section);	
+	$.table.setSections(sections);
+};
 
 var style;
 if (Ti.Platform.name === 'iPhone OS'){
@@ -30,7 +55,7 @@ if (Ti.Platform.name === 'iPhone OS'){
 $.activityIndicator.setStyle(style);
 $.activityIndicator.show();
 Alloy.Collections.news.fetch({
-    success: hideActivity,
+    success: loadNews,
     error: hideActivity
 });
 
